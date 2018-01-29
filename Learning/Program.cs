1d46f1a899c3be6f94879739c0d826fa3b9c9429
rikka-irk <rikka.irk@gmail.com>
2018-01-30 00:23:39 +0300

    Triangle

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -2,46 +2,118 @@
 
 namespace ConsoleApp1
 {
-
-    struct Student
+    class Point
     {
-        public string name;
-        public string surname;
-        public int group;
+        private double x;
+        private double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double GetX()
+        {
+            return x;
+        }
+
+        public double GetY()
+        {
+            return y;
+        }
+
+        public double GetDistanceTo(Point o)
+        {
+            return Math.Sqrt( Math.Pow(this.x - o.x,2) + Math.Pow(this.y - o.y, 2));
+        }
+
+
+        public override string ToString()
+        {
+            return "{" + x + "," + y + "}";
+        }
+
+        private static bool IsEqual (double a, double b)
+        {
+            double d = a * 0.00001;
+            return Math.Abs(a - b) < d;
+        }
+
+        public static bool operator ==(Point a, Point b)
+        {
+            return IsEqual(a.x,b.x) && IsEqual(a.y, b.y);
+        }
+
+        public static bool operator !=(Point a, Point b)
+        {
+            return !IsEqual(a.x, b.x) || !IsEqual(a.y, b.y);
+        }
+
     }
 
-    class Students
+    class Shape
     {
-        public Student[] students;
-        public int size;
+        private Point[] points;
 
-        public Students(int maxSize)
+        public Shape(Point[] points)
         {
-            this.size = 0;
-            this.students = new Student[maxSize];
+            this.points = points;
         }
 
-        public void add(Student student)
+        public Point GetPoint(int id)
         {
-            if (size < students.Length)
-            {
-                students[size] = student;
-                size = size + 1;
-            }
+            return points[id];
         }
 
-        public void findByGroupId(int groupId)
+        public int GetSize()
         {
-            for (int i = 0; i < this.size; ++i)
+            return points.Length;
+        }
+
+        public override string ToString()
+        {
+            string result = "[";
+
+            for (int i = 0; i<points.Length - 1; i++)
             {
-                if (groupId == this.students[i].group)
-                {
-                    Console.WriteLine(this.students[i].name);
-                }
-            }
+                result += points[i] + ", ";
+            } 
+            if (points.Length > 0)
+                result += points[points.Length-1];
+
+            return result + "]";
+        }   
+    }
+
+    class Triangle : Shape
+    {
+        public Triangle(Point a, Point b, Point c) : base(new Point[] { a, b, c })
+        {
+        }
+
+        public double GetAB()
+        {
+            return GetPoint(0).GetDistanceTo(GetPoint(1));
+        }
+        public double GetAC()
+        {
+            return GetPoint(0).GetDistanceTo(GetPoint(2));
+        }
+        public double GetBC()
+        {
+            return GetPoint(1).GetDistanceTo(GetPoint(2));
+        }
+
+        public bool IsValid()
+        {
+            return !(GetPoint(0) == GetPoint(1) || GetPoint(1) == GetPoint(2) || GetPoint(0) == GetPoint(2));
         }
 
     }
+
+    
+
 
 
     class Program
@@ -49,13 +121,9 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Students students = new Students(2);
-
-            Student student = new Student() { group = 7, name = "Ekaterina", surname = "" };
-            students.add(student);
-            students.findByGroupId(7);
-
-            ConsoleKeyInfo c = Console.ReadKey();
+            Triangle t = new Triangle(new Point(1, 2), new Point(1, 2), new Point(3, 4));
+            Console.WriteLine(t);
+            Console.WriteLine(t.IsValid());
         }
     }
 }
